af1f19e4fd9baf1b5c377a59990e3a182c686e84
Merovingio <60794624+RogerioLS@users.noreply.github.com>
2025-01-21 20:13:51 +0000

    terraform simple

--- main.tf
+++ main.tf
@@ -6,13 +6,3 @@ resource "aws_s3_bucket" "example" {
   bucket = var.bucket_name
   acl    = "private"
 }
-
-variable "region" {
-  description = "The AWS region to deploy resources"
-  default     = "us-east-1"
-}
-
-variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  default     = "example-bucket-terraform"
-}

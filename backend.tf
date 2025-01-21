88f0fc1648ce4285b4b1734ea3ba07d165e33bbb
Merovingio <60794624+RogerioLS@users.noreply.github.com>
2025-01-21 20:01:26 +0000

    terraform simple

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "772086365590-detect-plate-hml"  # Seu bucket do S3
-    key    = "terraform/state/${SOURCEREPONAME}-${SOURCEREPOBRANCH}.tfstate"  # Caminho do arquivo de estado
-    region = "us-east-1"  # Região da AWS
-    encrypt = true  # Habilita criptografia do estado
-  }
-}
--- main.tf
+++ main.tf
@@ -1,20 +1,18 @@
-data "aws_caller_identity" "current" {}
-module "ai_module" {
-  source = "./chatbot-module/infra/"
-  repository_branch   = var.repository_branch
-  aws_resource_prefix = var.aws_resource_prefix
+provider "aws" {
+  region = var.region
 }
 
-module "api_module" {
-  source = "./api-module/infra/"
-  repository_branch   = var.repository_branch
-  aws_resource_prefix = var.aws_resource_prefix
-  ai_lambda_name = module.ai_module.ai_lambda_out
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+  acl    = "private"
 }
 
-module "extract_images_trigger" {
-  source = "./extract-images-trigger/infra"
-  repository_branch   = var.repository_branch
-  aws_resource_prefix = var.aws_resource_prefix
-  lambda_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/brlink-elgin-ai-lambda-${var.repository_branch}"
+variable "region" {
+  description = "The AWS region to deploy resources"
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  default     = "example-bucket-terraform"
 }

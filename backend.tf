b80378e877d80038e7b4c5c43dca244fee72e0b0
Merovingio <60794624+RogerioLS@users.noreply.github.com>
2025-01-21 19:04:13 +0000

    test

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "517629291948-elgin-chatbot-hml"  # Seu bucket do S3
+    bucket = "772086365590-detect-plate-hml"  # Seu bucket do S3
     key    = "terraform/state/${SOURCEREPONAME}-${SOURCEREPOBRANCH}.tfstate"  # Caminho do arquivo de estado
     region = "us-east-1"  # Região da AWS
     encrypt = true  # Habilita criptografia do estado

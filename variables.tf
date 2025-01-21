6f7ca731ec24156e4f33102f3264a18eec44e1ea
Merovingio <60794624+RogerioLS@users.noreply.github.com>
2025-01-21 20:43:48 +0000

    terraform simple

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 
 variable "bucket_name" {
   description = "The name of the S3 bucket"
-  default     = "example-bucket-terraform"
+  default     = "example-bucket-terraform-teste"
 }
 
 variable "repository_branch" {
